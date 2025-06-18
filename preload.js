9d37d8fb64bb8a7d5f10dde9d95f3c9748763b2e
ayan9263091 <168567143+ayan9263091@users.noreply.github.com>
2025-06-18 23:54:12 +0500

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,7 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
+  openFile: () => ipcRenderer.invoke('dialog:openFile'),
   startCompression: (file, quality) => ipcRenderer.send('start-compression', file, quality),
-  onProgress: (cb)      => ipcRenderer.on('compression-progress', (e, pct) => cb(pct)),
-  onCompleted: (cb)     => ipcRenderer.on('compression-done',     (e, out) => cb(out))
+  onProgress: (cb)    => ipcRenderer.on('compression-progress', (e,p) => cb(p)),
+  onCompleted: (cb)   => ipcRenderer.on('compression-done',     (e,o) => cb(o))
 });
